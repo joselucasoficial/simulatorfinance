b858bba0d7c505728b0ee4cf2ba5d2df7c6142ae
Jose Lucas <joselucasoficial@gmail.com>
2021-11-03 19:32:22 -0300

    up names

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ function calculo() {
     var valor = document.getElementById('valor')
     var qw1 = window.document.getElementById('qw1')
 
-    var nomeVeiculo = String(veiculo.value)
-    var valorVeiculo = Number(valor.value)
-    var entrada = valorVeiculo / 2
+    var nVeiculo = String(veiculo.value)
+    var vVeiculo = Number(valor.value)
+    var entrada = vVeiculo / 2
     var parcelas = entrada / 60
-    var parcelasJuros = parcelas + (parcelas * (70/100))
+    var pJuros = parcelas + (parcelas * (70/100))
 
     qw1.innerHTML = `
-    <div>Carro : <strong>${nomeVeiculo}</strong></div>
+    <div>Carro : <strong>${nVeiculo}</strong></div>
     <div>Entrada: <strong>R$${entrada}</strong></div>
-    <div>+ 60x de <strong>R$${parcelasJuros.toFixed(2)}</strong></div>`
+    <div>+ 60x de <strong>R$${pJuros.toFixed(2)}</strong></div>`
+    
 }
